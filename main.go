5d0d91ac1c03d3544a216901a5919957e70e66fe
zhangtbj <zhangtbj@cn.ibm.com>
2022-03-25 09:41:10 +0800

    Change tecent to tencent

--- main.go
+++ main.go
@@ -63,7 +63,7 @@ func main() {
 	e.File("/img/azure_logo.png", "img/azure_logo.png")
 	e.File("/img/terraform_logo.png", "img/terraform_logo.png")
 	e.File("/img/alibaba_logo.png", "img/alibaba_logo.png")
-	e.File("/img/tecent_logo.png", "img/tecent_logo.png")
+	e.File("/img/tencent_logo.png", "img/tencent_logo.png")
 	e.File("/img/openshift_logo.png", "img/openshift_logo.png")
 	e.File("/", "index.html")
 	e.File("/hello", "views/hello.html")
